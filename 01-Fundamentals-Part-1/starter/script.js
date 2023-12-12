8fce6cd79128a1d5804f12dcdb8c338f9ff71290
mc-solo <revatoel@gmial.com>
2023-12-12 23:59:54 +0300

    setup check

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,9 +4,16 @@ let js = "amazing";
 if (js === "amazing") console.log("js is really amazing");
 
 // console.log("My name is Wondwosen Asegid");
-let firstName = "Wondwosen";
+const firstName = "Wondwosen";
 const lastName = "Asegid";
-
 const email = "wondwosen.asegid@gmial.com";
 
 console.log(`My name is ${firstName} ${lastName}`);
+
+// let, const and var
+
+let age = 20; //this can be mutated
+age = 21;
+
+const birthYear = 2002;
+birthYear = 2;
