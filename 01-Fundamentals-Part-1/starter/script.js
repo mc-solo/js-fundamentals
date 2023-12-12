8b32f397c43976c93d418a9ea02f4983b0bbdb2b
mc-solo <revatoel@gmial.com>
2023-12-12 23:48:40 +0300

    create script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let js = "amazing";
+if (js === "amazing") console.log("js is really amazing");
+
+// console.log("My name is Wondwosen Asegid");
+let firstName = "Wondwosen";
+const lastName = "Asegid";
+
+const email = "wondwosen.asegid@gmial.com";
+
+console.log(`My name is ${firstName} ${lastName}`);
