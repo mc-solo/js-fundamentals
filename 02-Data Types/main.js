57a98415ffc03ba575ab19e205f45755089ad623
Wondwosen Asegid <revatoel@gmail.com>
2023-12-13 20:15:11 +0300

    add dynamic typing

--- 02-Data Types/main.js
+++ 02-Data Types/main.js
@@ -3,4 +3,8 @@
 let javaScript = true;
 console.log(javaScript);
 
+console.log(typeof javaScript); //to check the data type of a var
+
+javaScript = "Yes";
+console.log(javaScript);
 console.log(typeof javaScript);
