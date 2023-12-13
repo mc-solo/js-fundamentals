cb5743a443d87e4948114917c66dad6ecc5b7270
Wondwosen Asegid <revatoel@gmail.com>
2023-12-13 21:16:30 +0300

    stict equivalence

--- 03-Operators/main.js
+++ 03-Operators/main.js
@@ -3,6 +3,16 @@ console.log(age1);
 
 const now = 2023;
 
+// math operators
+let a = 5;
+let b = 2;
+
+let sum = a + b; // Addition
+let difference = a - b; // Subtraction
+let product = a * b; // Multiplication
+let quotient = a / b; // Division
+let remainder = a % b; // Modulus (remainder after division)
+
 // double asterisk is exponentiation while the single one is considered multiplication
 console.log(age1 * 3);
 console.log(6 ** 3);
@@ -21,7 +31,10 @@ x += 27; //equals to x = x 10; also works for other operators
 
 console.log(x);
 
-// comparison operators
+// comparison operators return boolean value
 let z;
 let y;
-console.log(z > y); //boolean value
+console.log(z > y); //bool type result
+
+let isEqual = a === b; //strict equality
+let isNotEqual = a !== b; //strict inequality
