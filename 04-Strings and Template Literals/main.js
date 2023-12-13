f0fc900d04e9564446c3d1bb550bb246a3a0abd6
Wondwosen Asegid <revatoel@gmail.com>
2023-12-13 21:59:01 +0300

    template literals are cool

---
+++ 04-Strings and Template Literals/main.js
@@ -0,0 +1,15 @@
+// string interpolation
+let Name = "John";
+let greeting = `Hello, ${Name}`;
+console.log(greeting);
+
+// multiLineStrings
+let multiLineString = `This is a multiline string using a modern js
+                         feature called template literals
+                         `;
+// works perfectly with expressions
+
+let a = 40;
+let b = 7;
+const sum = a + b;
+console.log(`Sum: ${a + b}`);
