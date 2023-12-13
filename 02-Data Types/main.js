5be423139ae743e484100863a0c4b522d20da22c
Wondwosen Asegid <revatoel@gmail.com>
2023-12-13 20:31:06 +0300

    done checking for null dat type

--- 02-Data Types/main.js
+++ 02-Data Types/main.js
@@ -13,4 +13,10 @@ console.log(typeof javaScript);
 // undefined data types
 let varName;
 console.log(varName);
-console.log(typeof varName1);
+console.log(typeof varName);
+
+// null type
+
+let year = null;
+console.log(year);
+console.log(typeof year); //js bug, left for legacy reasons
