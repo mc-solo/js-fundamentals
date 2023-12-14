573c37e67b3913d18466b85490650194353a2e8b
Wondwosen Asegid <revatoel@gmail.com>
2023-12-14 19:49:08 +0300

    if-else statements

--- 05-Decision Making/main.js
+++ 05-Decision Making/main.js
@@ -1,7 +1,18 @@
+// if-else statements
 const x = 10;
 
 if (x >= 4) {
   console.log("x is greater than or equal to 10");
 } else {
   console.log("x is less than 10");
+}
+
+const age = 20;
+const isOldEnough = age >= 18;
+console.log(typeof isOldEnough);
+
+if (isOldEnough) {
+  console.log("You can get a driving license");
+} else {
+  console.log(`You're under age to get your driving license`);
 }
