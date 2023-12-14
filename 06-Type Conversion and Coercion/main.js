924daf6fc6731f0f7d80c0024153e6109877f4cf
Wondwosen Asegid <revatoel@gmail.com>
2023-12-14 20:31:53 +0300

    add some code changes

--- 06-Type Conversion and Coercion/main.js
+++ 06-Type Conversion and Coercion/main.js
@@ -2,9 +2,10 @@
 
 const x = "7";
 const y = 40;
-
 const sum = Number(x) + y;
 
 console.log(
-  `Type of x: ${typeof x}, Type of Number(x): ${typeof Number(x)}, sum: ${sum} `
+  `Type of x: ${typeof x}, Type of Number(x): ${typeof Number(x)}, sum: ${sum}`
 );
+
+// implicit conversion
