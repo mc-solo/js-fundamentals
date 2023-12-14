533f4f3d24609fdbe686ffcd5c55bf804c19363b
Wondwosen Asegid <revatoel@gmail.com>
2023-12-14 20:30:21 +0300

    playing with js type consversions

---
+++ 06-Type Conversion and Coercion/main.js
@@ -0,0 +1,10 @@
+// explicit conversions
+
+const x = "7";
+const y = 40;
+
+const sum = Number(x) + y;
+
+console.log(
+  `Type of x: ${typeof x}, Type of Number(x): ${typeof Number(x)}, sum: ${sum} `
+);
