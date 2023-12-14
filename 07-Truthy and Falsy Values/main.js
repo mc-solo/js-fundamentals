f858dbd2c1d48d3fe3e3be1fc3dfc360f04ddfb5
Wondwosen Asegid <revatoel@gmail.com>
2023-12-14 21:37:38 +0300

    boolean coercion

---
+++ 07-Truthy and Falsy Values/main.js
@@ -0,0 +1,7 @@
+// In JavaScript, values are broadly categorized as "truthy" or "falsy" based on their inherent boolean interpretation. These concepts come into play when JavaScript expects a boolean value, such as in conditional statements (if, while, etc.) or in logical operators.
+//
+
+const money = false;
+
+if (money) console.log(`Don't spend it all`);
+else console.log(`Go get a job`);
