7b9f41667c85ffb2f62c5652e3ad92185dc17c36
Wondwosen Asegid <revatoel@gmail.com>
2023-12-15 21:49:54 +0300

    expressions

--- 09-Statements and Expressions/main.js
+++ 09-Statements and Expressions/main.js
@@ -1,12 +1,17 @@
 //these are all statements
 // statements have specific purpose, they are use to perform some action
-
+// does'nt produce a value
 var x;
 
 x = 47;
 
-if (condition) {
+if (x) {
   // code
 } else {
   // code
 }
+
+// expressions are used to compute and/or produce value
+
+2 + 3;
+"2" + "3";
