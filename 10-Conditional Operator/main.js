d76bc430332b0d742c6d0f63bce118ca1d63aa2a
Wondwosen Asegid <revatoel@gmail.com>
2023-12-15 22:09:01 +0300

    fuckin' conditionals

--- 10-Conditional Operator/main.js
+++ 10-Conditional Operator/main.js
@@ -1,5 +1,10 @@
+// condition ? expression_if_true : expression_if_false;
+
 const age = 47;
 
 age >= 40
   ? console.log("Too old to live on this fucking earth")
   : console.log("remember we all are gonna die one day");
+
+let number;
+let isEven = number % 2 === 0 ? true : false;
