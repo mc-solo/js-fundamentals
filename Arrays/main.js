dd716d857c1114e0f5b8a14aae7b6b69f8222424
Wondwosen Asegid <revatoel@gmail.com>
2023-12-16 22:34:13 +0300

    playing with array structs

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,2 +1,7 @@
 let colors = ["red", "green", "blue"];
 console.log(colors);
+console.log(colors[2]);
+
+// let friends = newArray("me", "emsh", "zene", "mac");
+// console.log(friends);
+// console.log(friends(1));
