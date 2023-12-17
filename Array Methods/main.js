cb28e2253f52933c1614808511d3863e44d31eb7
Wondwosen Asegid <revatoel@gmail.com>
2023-12-17 21:06:39 +0300

    push and pop

---
+++ Array Methods/main.js
@@ -0,0 +1,11 @@
+const fruits = ["apple", "orange", "bannana"];
+console.log(fruits.length);
+
+// push adds element to the end of an array
+fruits.push("grape");
+console.log(fruits.length);
+console.log(fruits);
+
+// pop: removes the last element from the end of an array
+// fruits.pop();
+console.log(fruits.length, fruits);
