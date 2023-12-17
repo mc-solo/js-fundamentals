6317502883f90b3b0c7773185fde92d00de41d42
Wondwosen Asegid <revatoel@gmail.com>
2023-12-17 20:38:35 +0300

    ...

--- Arrays/main.js
+++ Arrays/main.js
@@ -4,11 +4,18 @@ console.log(colors[2]);
 
 //using array constructor
 
-let enmtyArray = new Array();
+let emptyArray = new Array();
 console.log(
   "*****************************************************************************************"
 );
 
-console.log(enmtyArray);
+console.log(emptyArray);
 let arrayWithLength = new Array(3);
 console.log();
+
+console.log(
+  "*****************************************************************************************"
+);
+
+console.log(emptyArray.length);
+console.log(colors[colors.length - 1]);
