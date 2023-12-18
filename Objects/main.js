01ee39f9fee5e8b1ae636ad8ef43bd7f0b78ee03
Wondwosen Asegid <revatoel@gmail.com>
2023-12-18 16:18:11 +0300

    done with objexts

--- Objects/main.js
+++ Objects/main.js
@@ -8,6 +8,22 @@ const person = {
 // order doesn't matter for objects
 console.log(person);
 
-// getting a property form an object
 console.log(); //blank space
-console.log(person.job);
+
+// getting a property form an object
+console.log(person.job); //dot notation
+console.log(person["lastName"]); //bracket notaion
+
+const nameKey = "Name";
+
+// the bracket notaion is cool when we want to compute the property name from other expression
+console.log(person["first" + nameKey]);
+console.log(person["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "what do you wanna know about me? choose between name, job, age"
+// );
+
+person.location = "Ethiopia";
+person.email = "wondwosen.asegid@gmail.com";
+console.log(person);
