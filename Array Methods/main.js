5faa23d6ce677ec7cb2275177735791b1ecc131e
Wondwosen Asegid <revatoel@gmail.com>
2023-12-18 20:17:33 +0300

    basics of the splice method

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -1,12 +1,10 @@
 const fruits = ["apple", "orange", "bannana"];
-console.log(fruits.length);
-
-// push adds element to the end of an array
 fruits.push("grape");
-console.log(fruits.length);
-console.log(fruits);
+fruits.unshift("papaya");
 
-// pop: removes the last element from the end of an array
-// fruits.pop();
-fruits.unshift("papaya"); //adds element to the beginning of the array
+console.log(fruits.length, fruits);
+
+//  the splice method
+
+fruits.splice(1.3);
 console.log(fruits.length, fruits);
