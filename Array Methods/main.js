6b7f440f6811b7c54d21946f1d0b3af8cbef9983
Wondwosen Asegid <revatoel@gmail.com>
2023-12-18 20:36:09 +0300

    experimenting with the slice method

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -1,10 +1,25 @@
-const fruits = ["apple", "orange", "bannana"];
-fruits.push("grape");
-fruits.unshift("papaya");
+// const fruits = ["apple", "orange", "bannana"];
+// console.log(fruits.length, fruits);
 
-console.log(fruits.length, fruits);
+// fruits.push("grape");
+// console.log(fruits.length, fruits);
 
-//  the splice method
+// fruits.unshift("papaya");
+// console.log(fruits.length, fruits);
 
-fruits.splice(1, 1);
-console.log(fruits.length, fruits);
+// //  the splice method
+
+// fruits.splice(1, 1);
+// console.log(fruits.length, fruits);
+
+// fruits.splice(3, 0, "mango", "melon");
+// console.log(fruits.length, fruits);
+
+// ....................................................
+
+const arr = ["a", "b", "c", "d", "e", "f"];
+7;
+const newArray = arr.slice(3);
+
+console.log(arr);
+console.log(newArray);
