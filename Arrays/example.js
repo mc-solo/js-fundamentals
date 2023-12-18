481f965316051af3e8a951fd16b536612400b0da
Wondwosen Asegid <revatoel@gmail.com>
2023-12-18 21:30:52 +0300

    join and split methods of an array

---
+++ Arrays/example.js
@@ -0,0 +1,12 @@
+let sentence = "This is a sample sentence.";
+
+let words = sentence.split(" ");
+console.log(words);
+console.log(typeof words);
+
+// the join method
+
+const fullName = ["wondwosen", "asegid"];
+let joined = fullName.join("|");
+console.log(joined);
+console.log(fullName);
