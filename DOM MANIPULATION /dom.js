590dd73da65826046c61d6964612f830be3bf3f9
Wondwosen Asegid <revatoel@gmail.com>
2023-12-19 20:50:23 +0300

    style elements through using dom manipulation

--- DOM MANIPULATION /dom.js
+++ DOM MANIPULATION /dom.js
@@ -9,3 +9,18 @@ console.log(header.textContent);
 // GET ELEMENTS BY CLASS
 let items = document.getElementsByClassName('list-item-group');
 console.log(items);
+
+// we just can't change the whole array at a time, instead we do this
+for (let i = 0; i < items.length; i++) {
+  console.log(items[i].textContent);
+}
+
+items[2].textContent = 'Second item of the list';
+
+// GET ELEMENTS BY TAG NANE
+
+let lsits = document.getElementsByTagName('li');
+
+for (let i = 0; i < lists.length; i++) {
+  console.log(list[i].style);
+}
