b5ec0c795fb0d864087c0d18da487784d8f9072a
Wondwosen Asegid <revatoel@gmail.com>
2023-12-20 00:22:58 +0300

    started again from w3 schools

--- DOM MANIPULATION /dom.js
+++ DOM MANIPULATION /dom.js
@@ -1,1 +1,10 @@
 // can;t understand this shit
+
+document.getElementById('primary-header');
+console.log(document.getElementById('primary-header'));
+
+document.getElementsByTagName('h1');
+console.log(document.getElementsByTagName('h1'));
+
+document.getElementsByClassName('list-item-group');
+console.log(document.getElementsByClassName('list-item-group'));
