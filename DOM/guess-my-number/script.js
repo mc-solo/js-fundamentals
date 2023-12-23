ed6438c4a7c934310d7650b57009eb2b9bb8562f
Wondwosen <revatoel@gmail.com>
2023-12-23 14:53:38 +0300

    manipulating value content with .value method

--- DOM/guess-my-number/script.js
+++ DOM/guess-my-number/script.js
@@ -2,12 +2,33 @@
 
 //the query selector
 let message =  document.querySelector('.message');
-console.log(message);
-
-//shows the text content that's found  inside the selected document node 
-console.log(message.textContent);
 
 
 // changing the text content using the .textContent method
 message.textContent = "Take your shot..."
+console.log(message);
+
+
+
+let score = document.querySelector('.label-score');
+console.log(score);
+// score.textContent = 13.5;
+
+
+// document.querySelector('.number').textContent = '14';
+// document.querySelector('.score').textContent = '9'
+
+let num = document.querySelector('.number');
+let value = num.value = 47;
+console.log(value);
+
+
+let guess  = document.querySelector('.guess')
+let guessValue = guess.value = 74;
+
+console.log(guessValue);
+
+
+
+
 
