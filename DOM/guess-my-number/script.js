95447bea8fb45003dd64476ee91268c8e3aa19f7
Wondwosen <revatoel@gmail.com>
2023-12-23 13:49:47 +0300

    change text content

--- DOM/guess-my-number/script.js
+++ DOM/guess-my-number/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
-let message =  document.querySelector('.message'); //the query selector
+//the query selector
+let message =  document.querySelector('.message');
 console.log(message);
 
+//shows the text content that's found  inside the selected document node 
 console.log(message.textContent);
+
+
+// changing the text content using the .textContent method
+message.textContent = "Take your shot..."
+
