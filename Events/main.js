d8d8dd4a30b123ffc22f928d7fda5f3c4c270ec8
Wondwosen <revatoel@gmail.com>
2023-12-31 10:02:52 +0300

    event targets

--- Events/main.js
+++ Events/main.js
@@ -6,7 +6,17 @@ const h2 = div.querySelector('h2');
 
 // console.log(view);
 // console.log(div);
-// console.log(h2);
+console.log(h2);
 
 
+const doSomething = function () {
+    alert('Doing something...')
+}
 
+h2.addEventListener('click', doSomething); //attaches an event listener for h2
+h2.removeEventListener('click', doSomething); //removes the event listener for h2
+
+h2.addEventListener('click', function(event) {
+    console.log('event.target');
+    event.target.textContent = 'Clicked'
+})
