176d0c4cc55497f087b8b073f54e933db93f0d07
Wondwosen <revatoel@gmail.com>
2023-12-31 09:31:57 +0300

    starter files

---
+++ Events/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const view = document.querySelector('#view2');
+const div = view.querySelector('div');
+const h2 = div.querySelector('h2');
+
+// console.log(view);
+// console.log(div);
+// console.log(h2);
+
+
+
