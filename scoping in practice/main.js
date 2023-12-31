2e55547bd8f19081d0c86d2a8f5fc719d3c093d0
Wondwosen <revatoel@gmail.com>
2023-12-31 21:41:56 +0300

    done with scoping

--- scoping in practice/main.js
+++ scoping in practice/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
 function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-    console.log(firstName); //looks the varaible in the next scope-works
+  const age = 2037 - birthYear;
+  // console.log(firstName); //looks the varaible in the next scope-works
 
+  function printAge() {
+    const output = `${firstName}, You'are ${age} years old`;
+    console.log(output);
 
-    function printAge() {
-        const output = `You'are ${age} years old`
-        console.log(output);
+    if (1991 <= birthYear <= 1996) {
+      const str = `oh and you're a millenial, ${firstName}`;
+      console.log(str);
     }
-    printAge();
-    return age;
+  }
+  printAge();
+  return age;
 }
 
 const firstName = 'Wondwosen';
-calcAge(2023)
- 
+calcAge(2023);
