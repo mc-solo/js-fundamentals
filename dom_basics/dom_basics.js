e2dbc2b5b836a32f0b15d4099ed8cbd81c66b5aa
Wondwosen <revatoel@gmail.com>
2024-04-13 02:13:25 +0300

    inner text and text content

--- dom_basics/dom_basics.js
+++ dom_basics/dom_basics.js
@@ -4,3 +4,6 @@ body.append('Hello world')
 
 const div = document.createElement('div')
 body.append(div)
+
+div.innerText = 'mc_solo' //works same as textContet
+div.textContent= 'Wondwosen Asegid' //over writes the  above one
