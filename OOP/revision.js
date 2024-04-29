d8e96c91f404d4ae757188493b11bc48feb4955b
wondwoesn <revatoel@gmail.com>
2024-04-30 00:14:24 +0300

    and object.create

--- OOP/revision.js
+++ OOP/revision.js
@@ -28,3 +28,18 @@ const page1 = new Book();
 
 const type = typeof page1;
 console.log(`type of page1 is ${page1}`);
+
+// come up with some examples
+function Student(name, college, department) {
+  this.name = name;
+  this.college = college;
+  this.department = department;
+}
+
+// in this case mc_solo is a new instance of Student
+const mc_solo = new Student();
+mc_solo.name = 'Wondwosen';
+mc_solo.age = '21';
+mc_solo.department = 'ComputerScience';
+
+console.log(mc_solo);
